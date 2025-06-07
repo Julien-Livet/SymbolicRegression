350cc1d08b90075066a2a9df41794125dd410103
Julien <julien.livet@free.fr>
2025-06-07 08:30:07 +0200

    Update required packages

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup
 
-setup(name='sr', 
-      version='1.0',
-      description='Symbolic regression',
-      author='Julien LIVET',
-      author_email='julien.livet@free.fr')
+setup(name = 'sr', 
+      version = '1.0',
+      description = 'Symbolic regression',
+      author = 'Julien LIVET',
+      author_email = 'julien.livet@free.fr',
+      install_requires = ['sympy', 'numpy'])
