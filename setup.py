a2c9e50d92b6fbdf0122dcfbf4993a906b7ff3c4
Julien <julien.livet@free.fr>
2025-06-07 20:01:10 +0200

    Update required packages

--- setup.py
+++ setup.py
@@ -5,4 +5,4 @@ setup(name = 'sr',
       description = 'Symbolic regression',
       author = 'Julien LIVET',
       author_email = 'julien.livet@free.fr',
-      install_requires = ['sympy', 'numpy', 'scipy'])
+      install_requires = ['sympy', 'numpy', 'scipy', 'deap'])
