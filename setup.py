e1b81249224e9455a084c843ea8cfde53f039ee9
Julien <julien.livet@free.fr>
2025-06-10 15:30:06 +0200

    Update

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name = 'sr',
       description = 'Symbolic regression',
       author = 'Julien LIVET',
       author_email = 'julien.livet@free.fr',
-      install_requires = ['sympy', 'numpy', 'scipy', 'deap', 'matplotlib', 'sklearn'],
-      setup_requires = ['sympy', 'numpy', 'scipy', 'deap', 'matplotlib', 'sklearn'])
+      install_requires = ['sympy', 'numpy', 'scipy', 'deap', 'matplotlib', 'scikit-learn'],
+      setup_requires = ['sympy', 'numpy', 'scipy', 'deap', 'matplotlib', 'scikit-learn'])
